8ca16527c6be28c87a14bf5ab46d352abbd97731
smnk-1 <semikinnikolay1@gmail.com>
2025-09-15 11:15:50 +0600

    fix: requirements

--- pages/3_ML models.py
+++ pages/3_ML models.py
@@ -32,7 +32,7 @@ st.header("Выбор моделей")
 selected_models = st.multiselect(
     "Выберите модели для предсказания",
     options=list(MODEL_PATHS.keys()),
-    default=list(MODEL_PATHS.keys())[0]
+    default=list(MODEL_PATHS.keys())[4]
 )
 
 st.header("Загрузка данных")
@@ -42,15 +42,15 @@ def manual_input():
     st.subheader("Ручной ввод признаков")
     cols = {}
     cols['vendor_id'] = st.selectbox("vendor_id", [1, 2], help="Идентификатор поставщика услуги")
-    cols['pickup_datetime'] = st.date_input("Дата и время начала поездки", datetime.now())
-    cols['pickup_time'] = st.time_input("Время начала поездки", datetime.now().time())
-    cols['dropoff_datetime'] = st.date_input("Дата и время окончания поездки", datetime.now())
-    cols['dropoff_time'] = st.time_input("Время окончания поездки", datetime.now().time())
+    cols['pickup_datetime'] = st.date_input("Дата и время начала поездки")
+    cols['pickup_time'] = st.time_input("Время начала поездки")
+    cols['dropoff_datetime'] = st.date_input("Дата и время окончания поездки")
+    cols['dropoff_time'] = st.time_input("Время окончания поездки")
     cols['passenger_count'] = st.number_input("Количество пассажиров", min_value=1, max_value=10, value=1)
-    cols['pickup_longitude'] = st.number_input("Долгота точки старта", format="%.6f")
-    cols['pickup_latitude'] = st.number_input("Широта точки старта", format="%.6f")
-    cols['dropoff_longitude'] = st.number_input("Долгота точки окончания", format="%.6f")
-    cols['dropoff_latitude'] = st.number_input("Широта точки окончания", format="%.6f")
+    cols['pickup_longitude'] = st.number_input("Долгота точки старта", 40.82707977294922, format="%.14f")
+    cols['pickup_latitude'] = st.number_input("Широта точки старта", -73.95169067382812, format="%.14f")
+    cols['dropoff_longitude'] = st.number_input("Долгота точки окончания", 40.72517013549805, format="%.6f")
+    cols['dropoff_latitude'] = st.number_input("Широта точки окончания", -73.9841537475586, format="%.14f")
 
     dt_start = datetime.combine(cols['pickup_datetime'], cols['pickup_time'])
     dt_end = datetime.combine(cols['dropoff_datetime'], cols['dropoff_time'])
@@ -107,16 +107,11 @@ if st.button("Получить предсказание"):
         st.success("Предсказания успешно выполнены")
         st.subheader("Результаты предсказаний")
         
-        # Создаем DataFrame для отображения результатов
         results_df = pd.DataFrame()
         
         for model_name, preds in results.items():
-            results_df[model_name] = [p for p in preds]
-        
-        # Добавляем индекс объектов
-        # results_df.index = [f"Объект {i+1}" for i in range(len(results_df))]
-        
-        # Отображаем таблицу с результатами
+            results_df[model_name] = [abs(p) for p in preds]
+
         st.dataframe(results_df.style.format("{:,.2f} s"))
         
     else:
